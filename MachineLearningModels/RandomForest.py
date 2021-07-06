3f3ad023db737c28b6fae2887b6e4c3652af9f2f
unknown <olarotimi@protonmail.com>
2021-07-06 16:44:43 +0100

    Random Forest Model

--- MachineLearningModels/RandomForest.py
+++ MachineLearningModels/RandomForest.py
@@ -19,5 +19,28 @@ plt.show()
 intrusion_Data['IPV4_SRC_ADDR'] = encoder().fit_transform(intrusion_Data['IPV4_SRC_ADDR'])
 intrusion_Data['IPV4_DST_ADDR'] = encoder().fit_transform(intrusion_Data['STANCE1'])
 intrusion_Data['Attack'] = encoder().fit_transform(intrusion_Data['Attack'])
+#Dropping Lable as its not need
+intrusion_Data = intrusion_Data.drop(['Label'],axis=1)
+
+# normarlising data to covert data range between 0-1
+
+cols_to_normarlize = ["IPV4_SRC_ADDR","L4_SRC_PORT","IPV4_DST_ADDR","L4_DST_PORT","PROTOCOL","L7_PROTO",
+                       "IN_BYTES","OUT_BYTES","IN_PKTS","OUT_PKTS","TCP_FLAGS","FLOW_DURATION_MILLISECONDS"
+                        ]
+
+scaler = MinMaxScaler()
+intrusion_Data[cols_to_normarlize] = scaler.fit_transform(intrusion_Data[cols_to_normarlize])
+
+# checking if data is normalised 
+print (intrusion_Data.head())
+print (intrusion_Data["Attack"].value_counts())
+
+
+#sepertaing lables and fetaures 
+
+x_data = intrusion_Data.drop('Attack', axis=1)
+
+#label has mutliple atrributes that must be determined later
+labels = intrusion_Data['Attack']
 
 
