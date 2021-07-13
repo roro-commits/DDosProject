c24f00bfaa06a4249e9ffd8a28ffb99b175ab820
unknown <olarotimi@protonmail.com>
2021-07-13 14:56:46 +0100

    existing React Web App Setup

--- MachineLearningModels/ReacWebApp/my-app/src/App.js
+++ MachineLearningModels/ReacWebApp/my-app/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import Header from "./UI/Header"
+import {ThemeProvider} from '@material-ui/core/styles';
+import {BrowserRouter,Route,Switch}  from 'react-router-dom'
+import  theme from "./UI/Theme"
+import ProfessorMMA from "./UI/fighterPage";
+import Statistics from "./UI/statistics";
+
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ThemeProvider theme ={theme}>
+     <BrowserRouter>
+     <Header value={value} setValue={setValue}  />
+      <Switch>
+      {/* <LandingPage /> */}
+        <Route exact path="/" component={()=> <div style={{height: "2000px"}} >Landing page</div>}/>
+        <Route exact path="/Project" component={ProfessorMMA }/>
+        <Route exact path="/chart" component={Statistics}/>
+        <Route exact path="/contact" component={()=> <div style={{height: "2000px"}} >contact</div> }/>
+      </Switch>
+     </BrowserRouter>
+   </ThemeProvider>
+      
+    
+  
   );
 }
 
