151ff7321ff9cf8807299fe65026f534bf3a4ec5
unknown <olarotimi@protonmail.com>
2021-07-24 23:52:56 +0100

    deploy test

--- app.py
+++ app.py
@@ -126,6 +126,6 @@ def react_api():
 if __name__ == "__main__":
     # app.run(debug=True)
     # app.run()
-     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT'))
 
 
