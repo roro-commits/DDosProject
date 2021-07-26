24cb4eaae1da1ec47b94ee1c9e5894f6191b29f6
unknown <olarotimi@protonmail.com>
2021-07-26 11:15:01 +0100

    heroku fix

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import os
 
 
 
-app = Flask(__name__, static_folder='./app/build',template_folder='..\\build', static_url_path='/')
+app = Flask(__name__, static_folder='/app/build',template_folder='..\\build', static_url_path='/')
 CORS(app, support_credentials=True)
 
 @app.route('/', methods=['GET', 'POST'])
